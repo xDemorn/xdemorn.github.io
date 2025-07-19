60f0e7a72b4cbe4eb2b9ac0d6a141fd276766a70
Mikolaj Jaworski <jaworski.miki98@gmail.com>
2025-07-19 18:51:21 +0200

    removed project

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function UUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = crypto.getRandomValues(new Uint8Array(1))[0] % 16;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8); // ensure 'y' is 8, 9, A, or B
-    return v.toString(16);
-  });
-}
